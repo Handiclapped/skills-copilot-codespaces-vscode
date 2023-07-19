3774afea9e8d5fe658e83020d3626b35211e68e1
Handiclapped <139421024+Handiclapped@users.noreply.github.com>
2023-07-19 03:37:07 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+  var member = {
+    name: 'John',
+    skills: ['js', 'css', 'html']
+  };
+  var skills = member.skills;
+  console.log(skills);
+  // ['js', 'css', 'html']
+}
